ac68db7d356c7967e3341d6c3d4eb66672110eac
Chaya Otikor <c.otikor@gmail.com>
2020-09-20 17:01:55 -0600

    algo currently extracts data hex from transactions with Birth topic

--- src/App.js
+++ src/App.js
@@ -5,83 +5,143 @@ import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import { Button, CircularProgress, LinearProgress } from "@material-ui/core";
 
 const useStyles = makeStyles({
   container: {
     border: "1px solid red",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   card: {
     backgroundColor: "pink",
     width: "200px",
     height: "200px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  button: {
+    width: "50%",
+    fontSize: "12px",
+  },
+  progress: {
+    height: '50px',
+    width: '50px',
+    color: 'red'
+  }
 });
 
 function App() {
   const classes = useStyles();
 
   const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
-    loadTransactions();
-  });
-
-  const loadTransactions = async () => {
+  const loadData = async (startingBlock, endingBlock) => {
     const web3 = new Web3(process.env.REACT_APP_URL);
     const birthTopic = web3.utils.sha3(
       "Birth(address,uint256,uint256,uint256,uint256)"
     );
-    const address = process.env.REACT_APP_CONTRACT_ADDRESS
-    const resultArr = []
-    
-    for (let i = 6607985; i <= 7028323; i += 20001){
-      console.log(i, resultArr)
+    const address = process.env.REACT_APP_CONTRACT_ADDRESS;
+    const resultArr = [];
+
+    for (let i = startingBlock; i <= endingBlock; i += 10001) {
+      console.log(i, resultArr.length);
+      if (i + 10001 > endingBlock) {
+        try {
+          await web3.eth
+            .getPastLogs({
+              fromBlock: i,
+              toBlock: endingBlock,
+              address: address,
+              topics: [birthTopic],
+            })
+            .then(async (res) => {
+              for (let j = 0; j < res.length; j++) {
+                resultArr.push(res[j].data)
+              }
+            });
+          console.log("last block success");
+        } catch (err) {
+          if (
+            err.message ===
+            "Returned error: query returned more than 10000 results"
+          ) {
+            console.log("last block err");
+          }
+        }
+        break;
+      }
       try {
-         resultArr.push(await web3.eth
+        await web3.eth
           .getPastLogs({
             fromBlock: i,
-            toBlock: i + 20000,
+            toBlock: i + 10000,
             address: address,
             topics: [birthTopic],
-          }))
-      } catch(err){
-        if (err.message === 'Returned error: query returned more than 10000 results') {
-          resultArr.push(
-            await web3.eth.getPastLogs({
+          })
+          .then(async (res) => {
+              for (let j = 0; j < res.length; j++) {
+                resultArr.push(res[j].data);
+              }
+          });
+      } catch (err) {
+        if (
+          err.message ===
+          "Returned error: query returned more than 10000 results"
+        ) {
+          await web3.eth
+            .getPastLogs({
               fromBlock: i,
               toBlock: i + 1000,
               address: address,
               topics: [birthTopic],
             })
-          );
-          i -= 19000
+            .then(async (res) => {
+              for (let j = 0; j < res.length; j++) {
+                resultArr.push(res[j].data);
+              }
+            });
+          i -= 9000;
         }
       }
-       
-        if (i + 20001 > 7028323) {
-          console.log("last start", (7028323 - (7028323 - i + 1)));
-          
-          break;
-        }
     }
-    setTransactions(transactions => [...transactions, resultArr])
-        console.log(transactions);
+    return resultArr
+    // setTransactions((transactions) => [...transactions, resultArr]);
+    // console.log(transactions);
   };
+
+  const decodeTransactions = () => {
+
+  }
 
   return (
     <Container className={classes.container}>
-      <Card className={classes.card}>
+      <Button
+        className={classes.button}
+        onClick={async (e) => {
+          e.preventDefault();
+          setLoading(true)
+          await loadData(6607985, 7028323);
+          setLoading(false);
+        }}
+      >
+        Load Transactions
+      </Button>
+      {
+        loading === true ? <CircularProgress className={classes.progress}/> : <Card className={classes.card}>
         <CardContent>
           <Typography variant="caption" className="text">
-            Transaction Number
+            Data
           </Typography>
         </CardContent>
       </Card>
+      }
+
     </Container>
   );
 }
 
 export default App;
-
-/* Need to map over transation logs to find the address
- */
