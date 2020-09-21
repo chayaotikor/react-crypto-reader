34d6fb6555e3abe6fbe1ad6c05f7be83af250217
Chaya Otikor <c.otikor@gmail.com>
2020-09-20 22:42:20 -0600

    more styling, refactored code to one file, data presentation changes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,109 @@
 import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import abi from './abi'
-import KittyList from './components/KittyList'
 import { makeStyles } from "@material-ui/core/styles";
-import {Route} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import moment from "moment";
 import {
   Container,
+  Grid,
   Card,
-  CardContent,
+  Typography,
   Button,
   CardMedia,
   CircularProgress,
-  Typography,
+  FormControl,
+  TextField,
 } from "@material-ui/core";
+
+
 
 
 const useStyles = makeStyles({
   container: {
+    border: "1px solid #52154e",
+    display: "flex",
+    flexFlow: "column nowrap",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100vw",
+    backgroundColor: "#f9cff2",
+
+    padding: 0,
+    height: "100vh",
+  },
+  cardContainer: {
+    flexFlow: "row wrap",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderBottom: "1px solid #52154e",
+    height: "40%",
+    width: "100%",
+
+    overflowY: "scroll",
+  },
+  bottomContainer: {
+    display: "flex",
+    flexFlow: "column nowrap",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    height: "60%",
+    width: "100%",
+    overflowY: "scroll",
+  },
+  progressContainer: {
     display: "flex",
     flexFlow: "column nowrap",
     alignItems: "center",
     justifyContent: "center",
-    height: "100vh",
-    backgroundColor: "#EFE9E7",
   },
   card: {
-    backgroundColor: "pink",
+    width: "100px",
+    height: "100px",
+    display: "flex",
+    flexFlow: "column nowrap",
+    alignItems: "center",
+    margin: "5px 0",
+    justifyContent: "space-around",
+    backgroundColor: "#52154E",
+  },
+  bigCard: {
     width: "200px",
     height: "200px",
     display: "flex",
     flexFlow: "column nowrap",
     alignItems: "center",
-    justifyContent: "center",
+    margin: "5px 0",
+    justifyContent: "flex-start",
+    backgroundColor: "#52154E",
+    overflowY: 'scroll'
+  },
+  buttonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+    padding: '5px'
   },
   button: {
-    width: "90%",
+    width: "45%",
     fontSize: "12px",
+    margin: "5px",
+    backgroundColor: "#52154e",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "rgba(82, 21, 78, .5)",
+    },
+  },
+  form: {
+    display: "flex",
+    flexFlow: "row nowrap",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    width: "45%",
+    marginTop: "10px",
   },
   progress: {
     height: "50px",
@@ -43,25 +111,92 @@ const useStyles = makeStyles({
     color: "#111344",
   },
   image: {
-    height: 0,
-    paddingTop: "56.25%", // 16:9
+    height: "50px",
+    width: "50px",
+    paddingTop: "56.25%",
+  },
+  bigImage: {
+    minHeight: "100px",
+    width: "100px",
+  },
+  idText: {
+    fontSize: "12px",
+    color: "white",
   },
 });
 
 function App() {
   const classes = useStyles();
+
+  /* WEB3 Constants */
   const web3 = new Web3(process.env.REACT_APP_URL);
   const birthTopic = web3.utils.sha3(
     "Birth(address,uint256,uint256,uint256,uint256)"
   );
   const address = process.env.REACT_APP_CONTRACT_ADDRESS;
-  let contract = new web3.eth.Contract(abi, address)
+  const contract = new web3.eth.Contract(abi, address);
 
+  /* STATE */
   const [birthData, setBirthData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [mostBirths, setMostBirths] = useState({id:null})
+  const [mostBirths, setMostBirths] = useState({ id: null });
+  const [errorMessage, setErrorMessage] = useState("");
+  const [startingBlock, setStartingBlock] = useState("");
+  const [endingBlock, setEndingBlock] = useState("");
+  const [errorInput1, setErrorInput1] = useState(false);
+  const [helperText1, setHelper1] = useState(false);
+  const [errorInput2, setErrorInput2] = useState(false);
+  const [helperText2, setHelper2] = useState(false);
+  const [toggleMother, setToggleMother] = useState(false);
 
+  /* METHODS */
+  const loadList = async (startingBlock, endingBlock) => {
+    const resultArr = [];
+    if (!startingBlock || !endingBlock) {
+      if (!startingBlock) {
+        setErrorInput1(true);
+        setHelper1("Block number needed.");
+        setErrorMessage("Block number needed.");
+      }
+      if (!endingBlock) {
+        setErrorInput2(true);
+        setHelper2("Block number needed.");
+        setErrorMessage("Block number needed.");
+      }
+    } else {
+      for (let i = startingBlock; i <= endingBlock; i++) {
+        try {
+          await web3.eth
+            .getPastLogs({
+              fromBlock: i,
+              toBlock: i,
+              address: address,
+              topics: [birthTopic],
+            })
+            .then(async (res) => {
+              for (let j = 0; j < res.length; j++) {
+                let result = await web3.eth.abi.decodeLog(
+                  [
+                    { indexed: false, name: "owner", type: "address" },
+                    { indexed: false, name: "kittyId", type: "uint256" },
+                    { indexed: false, name: "matronId", type: "uint256" },
+                    { indexed: false, name: "sireId", type: "uint256" },
+                    { indexed: false, name: "genes", type: "uint256" },
+                  ],
+                  res[j].data,
+                  [birthTopic]
+                );
+                resultArr.push(result);
+              }
+            });
+        } catch (err) {
+          throw new Error(err);
+        }
+      }
+    }
 
+    setBirthData([...resultArr]);
+  };
 
   const findMostBirths = async () => {
     const matronIdCount = {};
@@ -74,53 +209,199 @@ function App() {
         } else {
           matronIdCount[birthData[i].matronId] = 1;
         }
-        if (matronIdCount[birthData[i].matronId] > mostBirthsCount) {
+        if (matronIdCount[birthData[i].matronId] >= mostBirthsCount) {
           mostBirthsCount = matronIdCount[birthData[i].matronId];
           mostBirths = birthData[i].matronId;
         }
       } catch (err) {
-        throw new Error(err)
+        throw new Error(err);
       }
     }
-    console.log(mostBirths)
     let result = await contract.methods.getKitty(mostBirths).call();
-    setMostBirths({ id: mostBirths, ...result })
+    setMostBirths({ id: mostBirths, ...result });
+  };
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (e.target.name === "startingBlock") {
+      setStartingBlock(e.target.value);
+      if (isNaN(Number(e.target.value)) && e.target.value !== "") {
+        setErrorInput1(true);
+        setHelper1("Input must be an integer.");
+      } else {
+        setErrorInput1(false);
+        setHelper1(null);
+      }
+    }
+    if (e.target.name === "endingBlock") {
+      setEndingBlock(e.target.value);
+      if (isNaN(Number(e.target.value)) && e.target.value !== "") {
+        setErrorInput2(true);
+        setHelper2("Input must be an integer.");
+      } else {
+        setErrorInput2(false);
+        setHelper2(null);
+      }
+    }
   };
 
   return (
     <Container className={classes.container}>
-      <Route
-        exact
-        path="/"
-        render={(props) => (
-          <KittyList setLoading={setLoading} birthData={birthData} web3={web3} birthTopic={birthTopic} address={address} loading={loading} setBirthData={setBirthData} />
-        )}
-      />
-      {/* <Card className={classes.card}>
-        <CardContent>
-          {loading === true ? (
+      <Typography variant="caption" className={classes.idText}>
+        Total Kitties Born: {birthData.length}
+      </Typography>
+      <Grid container className={classes.cardContainer}>
+        {loading === true ? (
+          <Grid item className={classes.progressContainer}>
+            <Typography variant="caption" className={classes.idText}>
+              Searching for Kitties...
+            </Typography>
             <CircularProgress className={classes.progress} />
-          ) : (
-            <CardMedia
-              src={`https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/${mostBirths.id}.svg`}
-              className={classes.image}
-            ></CardMedia>
+          </Grid>
+        ) : (
+          birthData.map((kitty) => (
+            <Card className={classes.card} key={kitty.kittyId}>
+              <CardMedia
+                image={`https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/${kitty.kittyId}.svg`}
+                className={classes.image}
+              />
+              <Link
+                to={{
+                  pathname: `https://www.cryptokitties.co/kitty/${kitty.kittyId}`,
+                }}
+                target="_blank"
+                style={{ textDecoration: "underline" }}
+              >
+                <Typography variant="caption" className={classes.idText}>
+                  ID: {kitty.kittyId}
+                </Typography>
+              </Link>
+            </Card>
+          ))
+        )}
+      </Grid>
+      <Grid className={classes.bottomContainer}>
+        <FormControl className={classes.form}>
+          <TextField
+            variant="outlined"
+            inputProps={{
+              style: { fontSize: "12px" },
+            }}
+            InputLabelProps={{
+              style: {
+                fontSize: "12px",
+                color: "black",
+              },
+            }}
+            FormHelperTextProps={{ style: { fontSize: "8px" } }}
+            className={classes.input}
+            label="Starting Block"
+            name="startingBlock"
+            placeholder="eg. 6507985"
+            value={startingBlock}
+            size="small"
+            onChange={(e) => handleChange(e)}
+            error={errorInput1}
+            helperText={helperText1}
+          />
+          <TextField
+            variant="outlined"
+            inputProps={{
+              style: { fontSize: "12px" },
+            }}
+            InputLabelProps={{
+              style: {
+                fontSize: "12px",
+                color: "black",
+              },
+            }}
+            FormHelperTextProps={{ style: { fontSize: "8px" } }}
+            className={classes.input}
+            label="Ending Block"
+            name="endingBlock"
+            placeholder="eg. 6509440"
+            value={endingBlock}
+            size="small"
+            onChange={(e) => handleChange(e)}
+            error={errorInput2}
+            helperText={helperText2}
+          />
+        </FormControl>
+        <Grid className={classes.buttonContainer}>
+          <Button
+            className={classes.button}
+            onClick={async (e) => {
+              e.preventDefault();
+              setToggleMother(false);
+              setLoading(true);
+              await loadList(startingBlock, endingBlock);
+              setLoading(false);
+              setStartingBlock("");
+              setEndingBlock("");
+            }}
+          >
+            Find Kitties
+          </Button>
+          {birthData.length === 0 ? null : (
+            <Button
+              className={classes.button}
+              onClick={async (e) => {
+                e.preventDefault();
+                setToggleMother(true);
+                await findMostBirths();
+              }}
+            >
+              Find Momma Kitty
+            </Button>
           )}
-        </CardContent>
-        <Button
-          className={classes.button}
-          onClick={async (e) => {
-            e.preventDefault();
-            setLoading(true);
-            await findMostBirths();
-            setLoading(false);
-          }}
-        >
-          Find Most Births
-        </Button>
-      </Card> */}
+        </Grid>
+        {toggleMother === false ? null : mostBirths.id === null ? (
+          <Grid item className={classes.progressContainer}>
+            <Typography variant="caption" className={classes.idText}>
+              Calculating Momma Kitty...
+            </Typography>
+            <CircularProgress className={classes.progress} />
+          </Grid>
+        ) : (
+          <Card className={classes.bigCard}>
+            <CardMedia
+              image={`https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/${mostBirths.id}.svg`}
+              className={classes.bigImage}
+            />
+            <Link
+              to={{
+                pathname: `https://www.cryptokitties.co/kitty/${mostBirths.id}`,
+              }}
+              target="_blank"
+              style={{ textDecoration: "underline" }}
+            >
+              <Typography variant="caption" className={classes.idText}>
+                ID: {mostBirths.id}
+              </Typography>
+            </Link>
+            <Typography variant="caption" className={classes.idText}>
+              Generation: {mostBirths.generation}
+            </Typography>
+            <Typography variant="caption" className={classes.idText}>
+              Birthday: {mostBirths.birthTime}
+            </Typography>
+            <Link
+              to={{
+                pathname: `https://kittycalc.co/read/?k1=${mostBirths.id}&k2=461679`,
+              }}
+              target="_blank"
+              style={{ textDecoration: "underline" }}
+            >
+              <Typography variant="caption" className={classes.idText}>
+                Genes
+              </Typography>
+            </Link>
+          </Card>
+        )}
+      </Grid>
     </Container>
   );
 }
+
 
 export default App;
